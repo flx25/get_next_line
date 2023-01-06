1f8d644141babc928d45cb5d52e08ae39e191f58
Felix von Nagel <fvon-nag@c2s16d93.42wolfsburg.de>
2023-01-06 14:48:48 +0100

    some trieds updated header

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 09:47:32 by fvon-nag          #+#    #+#             */
-/*   Updated: 2023/01/06 10:45:18 by fvon-nag         ###   ########.fr       */
+/*   Updated: 2023/01/06 14:46:37 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,61 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int	findnewline(int fd)
+char	*findnewline(char *out, int i)
 {
-	int		c;
+	char	*newout;
+	int 	c;
 
 	c = 0;
-	while (fd + c != '\n')
+
+	newout = malloc(50 * sizeof(char));
+	while (out[i] != '\n' )
 	{
+		newout[c] = out[i];
+		i++;
 		c++;
 	}
-	return (c);
+	newout[c] = out[i];
+	return (newout);
+}
+
+int	checknl(char *out)
+{
+	int	i;
+
+	i = 0;
+
+	while (out[i] != '\n' && out[i] != '\0')
+	{
+		i++;
+	}
+	if (out[i] == '\n')
+		return (1);
+	else
+		return (0);
 }
 
 char	*get_next_line(int fd)
 {
-	char	*out;
+	char		*out;
+	static int	i;
 
 	out = calloc(50, sizeof(char));
-
-	read(fd, out, findnewline(fd));
-
-	return (out);
+	if (checknl(out) == 0)
+		read(fd, out, BUFFER_SIZE);
+	return (findnewline(out, i));
 }
 
 int	main(void)
 {
+
 	int	fd_to_read;
+	char	*out;
 
 	fd_to_read = open("Testtext.txt", O_RDONLY);
+	out = get_next_line(fd_to_read);
+	printf("%s", out);
+	printf("%s", get_next_line(fd_to_read));
+	printf("%s", get_next_line(fd_to_read));
 	printf("%s", get_next_line(fd_to_read));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,18 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 09:43:40 by fvon-nag          #+#    #+#             */
-/*   Updated: 2023/01/06 09:45:47 by fvon-nag         ###   ########.fr       */
+/*   Updated: 2023/01/06 14:20:06 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
 char	*get_next_line(int fd);
+
 
 #endif
