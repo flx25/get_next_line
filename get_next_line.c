9ce58ad662938b8b6df045e6e34ef19151a2030e
Felix von Nagel <fvon-nag@c2s16d93.42wolfsburg.de>
2023-01-09 10:34:54 +0100

    saved zwischenstate not working outside of debugger

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 09:47:32 by fvon-nag          #+#    #+#             */
-/*   Updated: 2023/01/07 14:40:49 by fvon-nag         ###   ########.fr       */
+/*   Updated: 2023/01/09 10:31:58 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,14 +61,16 @@ char	*transfer(char *out, int *pi, int transfersize)
 	char	*newout;
 	int		c;
 	int		*i;
+	int		end;
 
 	if (transfersize == 0)
 		transfersize = ft_strlen(out);
 
 	i = pi;
 	c = 0;
+	end = transfersize - *i;
 	newout = malloc(transfersize * sizeof(char));
-	while (c < transfersize)
+	while (c < end)
 	{
 		newout[c] = out[*i];
 		(*i)++;
@@ -80,47 +82,42 @@ char	*transfer(char *out, int *pi, int transfersize)
 
 int	checknl(char *out, int i, int *ptransfersize)
 {
-	while (out[i] != '\n' && out[i] != '\0')
+	while (i < ft_strlen(out) && out[i] != '\n')
 	{
 		i++;
 	}
-	if (out[i] == '\n')
+	if (i < ft_strlen(out) && out[i] == '\n')
 	{
 		*ptransfersize = i + 1;
 		return (1);
 	}
-	if (out[i] != '\n' && i == 0)
-		return (-1);
 	else
 		return (0);
 }
 
 char	*get_next_line(int fd)
 {
-	char			*whole;
-	char			temp[BUFFER_SIZE + 1];
+	static char		*whole;
+	static char		temp[BUFFER_SIZE + 1];
 	static int		i;
 	int				transfersize;
 	int				readstat;
 
-	whole = malloc(BUFFER_SIZE + 1 * sizeof(char));
-	readstat =  read(fd, temp, BUFFER_SIZE);
-	if (readstat <= 0)
-	{
-		free(whole);
+	if(!whole)
+		whole = malloc(BUFFER_SIZE + 1 * sizeof(char));
+	readstat = read(fd, temp, BUFFER_SIZE);
+	if (readstat <= 0 && checknl(whole, i, &transfersize) == 0 && checknl(temp, i, &transfersize) == 0)
 		return (NULL);
-	}
+
 	else if (readstat < BUFFER_SIZE)
 	{
-		free(whole);
-		temp[BUFFER_SIZE] = '\0';
 		checknl(temp, i, &transfersize);
 		return (transfer(temp, &i, transfersize));
 	}
 	else
 	{
 	whole = ft_strjoin(whole, temp);
-		while (checknl(whole, i, &transfersize) == 0)
+		while (checknl(whole, 0, &transfersize) == 0 && readstat == BUFFER_SIZE)
 		{
 			readstat = read(fd, temp, BUFFER_SIZE);
 			if (readstat < BUFFER_SIZE && readstat != 0)
@@ -136,15 +133,15 @@ char	*get_next_line(int fd)
 	}
 }
 
-// int	main(void)
-// {
-// 	int		fd_to_read;
-// 	char	*out;
+int	main(void)
+{
+	int		fd_to_read;
+	char	*out;
 
-// 	fd_to_read = open("Testtext.txt", O_RDONLY);
-// 	out = get_next_line(fd_to_read);
-// 	printf("%s", out);
-// 	printf("%s", get_next_line(fd_to_read));
-// 	printf("%s", get_next_line(fd_to_read));
-// 	printf("%s", get_next_line(fd_to_read));
-// }
+	fd_to_read = open("Testtext.txt", O_RDONLY);
+	out = get_next_line(fd_to_read);
+	printf("%s", out);
+	printf("%s", get_next_line(fd_to_read));
+	printf("%s", get_next_line(fd_to_read));
+	printf("%s", get_next_line(fd_to_read));
+}
