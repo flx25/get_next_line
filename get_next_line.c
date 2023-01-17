79f9c356873246fd1fde5f0a8b87eafc68d6361a
Felix Von nagel <fvon-nag@c2s16d93.42wolfsburg.de>
2023-01-17 11:26:39 +0100

    pre rebuild again

--- get_next_line.c
+++ get_next_line.c
@@ -185,28 +185,30 @@ char	*ft_realloc(char *str, int size)
 
 char	*ft_readandsearch(int fd, char *buffer, char *line)
 {
-	int		readstat;
-	int		index;
-	int		readcount;
+	int			readstat;
+	int			index;
+	static int	readcount;
 
 	readstat = 1;
 	index = 0;
-	readcount = 0;
 	while (!ft_strchr(buffer, '\n') && readstat != 0)
 	{
 		line = ft_realloc(line, strlen(line) + BUFFER_SIZE + 2);
+		ft_memmove(line + strlen(line), buffer, strlen(buffer));
 		readstat = read(fd, buffer, BUFFER_SIZE);
+		buffer[readstat] = '\0';
 		if (readstat == 0 && readcount == 0)
 			return (free(line), NULL);
-
-		buffer[readstat] = '\0';
-		ft_memmove(line + strlen(line), buffer, strlen(buffer));
 		readcount++;
 	}
 	index = ft_strchridx(buffer, '\n');
 	if(index != -1)
+	{
+		ft_memmove(line + strlen(line), buffer, index +1);
 		ft_memmove(buffer, buffer + index + 1, ft_strlen(buffer) - (index));//copy rest of buffer to the beginning of buffer - how to not copy the 0 termi
-
+	}
+	if (!line[0])
+		return (free(line), NULL);
 	return (line);
 }
 
@@ -223,16 +225,16 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-int	main(void)
-{
-	int		fd_to_read;
-	char	*out;
+// int	main(void)
+// {
+// 	int		fd_to_read;
+// 	char	*out;
 
-	fd_to_read = open("41_with_nl", O_RDONLY);
-	out = get_next_line(fd_to_read);
-	printf("%s", out);
-	printf("%s", get_next_line(fd_to_read));
-	printf("%s", get_next_line(fd_to_read));
-	printf("%s", get_next_line(fd_to_read));
-	close(fd_to_read);
-}
+// 	fd_to_read = open("41_with_nl", O_RDONLY);
+// 	out = get_next_line(fd_to_read);
+// 	printf("%s", out);
+// 	printf("%s", get_next_line(fd_to_read));
+// 	printf("%s", get_next_line(fd_to_read));
+// 	printf("%s", get_next_line(fd_to_read));
+// 	close(fd_to_read);
+// }
